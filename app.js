42403f9a1fff7a53aac2e1e2f2e4fd9cf394826d
Vitor Lobo Muniz <vitolrobinho@hotmail.com>
2023-03-16 23:52:36 -0300

    teste2

--- app.js
+++ app.js
@@ -6,10 +6,8 @@ app.get('/', (req, res) => {
   return res.json({ message: 'server is up' })
 })
 
-app.post('/test', (req, res) => {
-  const { name } = req.body;
-
-  return res.json({ name })
+app.get('/atualizar', (req, res) => {
+  return res.json({ message: 'server atualizado' })
 })
 
 app.listen(port, () => console.log(`server is running on ${port}`))
