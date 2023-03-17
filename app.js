1a0d457d0db87d086d24f32b423318620f910574
Vitor Lobo Muniz <vitolrobinho@hotmail.com>
2023-03-16 23:46:05 -0300

    tentando post no servidor

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const app = express()
 const port = 3333
 
 app.get('/', (req, res) => {
-  return res.json({message:'server is up'})
+  return res.json({ message: 'server is up' })
+})
+
+app.post('/test', (req, res) => {
+  const { name } = request.body;
+
+  return res.json({name})
 })
 
 app.listen(port, () => console.log(`server is running on ${port}`))
